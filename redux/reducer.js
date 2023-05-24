74f0360a816f9bb113c290781e05a92d942aea81
JuanMaCarrearas <manuucarreras@gmail.com>
2023-05-24 20:43:54 -0300

    redux

---
+++ redux/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  movie: 20
+
+}
+
+function rootReducer (state = initialState, action) {
+  switch (action.type) {
+    default : return state
+  }
+}
+
+export default rootReducer
