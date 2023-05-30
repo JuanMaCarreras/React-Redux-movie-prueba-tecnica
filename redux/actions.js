38122f7a20b56e1d324c1d6bcdca0d77037f21e2
JuanMaCarrearas <manuucarreras@gmail.com>
2023-05-29 23:04:27 -0300

    actions listas

--- redux/actions.js
+++ redux/actions.js
@@ -1,9 +1,9 @@
+export const setPage = (pageNumber) => ({
+  type: 'SET_PAGE',
+  payload: pageNumber
+})
+
 export const setMoviesPerPage = (perPage) => ({
   type: 'SET_MOVIES_PER_PAGE',
   payload: perPage
-})
-
-export const PerPage = (pageNumber) => ({
-  type: 'SET_PAGE_NUMBER',
-  payload: pageNumber
 })
