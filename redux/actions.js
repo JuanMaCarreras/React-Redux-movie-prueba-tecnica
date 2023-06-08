3752bb2e798bb420f6df2308d051da8f9cc4b7e2
JuanMaCarrearas <manuucarreras@gmail.com>
2023-06-07 23:42:56 -0300

    .

--- redux/actions.js
+++
@@ -1,9 +0,0 @@
-export const setPage = (pageNumber) => ({
-  type: 'SET_PAGE',
-  payload: pageNumber
-})
-
-export const setMoviesPerPage = (perPage) => ({
-  type: 'SET_MOVIES_PER_PAGE',
-  payload: perPage
-})
