a5b23e3761401fe6d1dc240c365c1611c108fe12
JuanMaCarrearas <manuucarreras@gmail.com>
2024-04-25 18:07:51 -0300

    render App component

--- main.jsx
+++ main.jsx
@@ -1,13 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import store from './redux/store'
-import App from './App'
+import store from './src/redux/store'
+import React from 'react'
+import App from './App.jsx'
 
 const root = createRoot(document.getElementById('app'))
 
 root.render(
   <Provider store={store}>
-    <App />
-  </Provider>
 
+    <App />
+
+  </Provider>
 )
