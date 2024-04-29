e77b9bb9efcb8567d50f461dfc65919584080b20
JuanMaCarrearas <manuucarreras@gmail.com>
2024-04-29 18:31:44 -0300

    Styles

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+    background: black;
+    border-radius: 1rem;
+    cursor: pointer;
+    padding-inline: 1rem;
+    margin: 9px;
+    color: white;
+
+`
+
+export default Button
